af64a6ab7dbdfd7fd60762bc1fad0698887d166a
Vishv0407 <vishvkumar.b@ahduni.edu.in>
2024-11-26 21:59:51 +0530

    more update

--- src/data.js
+++ src/data.js
@@ -1,3 +1,5 @@
+require('dotenv').config({ path: '../Backend/.env' });
+
 export const frontend = '';
 // export const backend = 'http://localhost:4000';
 export const backend = process.env.BACKEND_URL;
