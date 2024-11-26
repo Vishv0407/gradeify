232bf77e16b6557c7d59475b8fbb0df26ef2c907
Vishv0407 <vishvkumar.b@ahduni.edu.in>
2024-11-26 22:16:50 +0530

    back to normal

--- src/data.js
+++ src/data.js
@@ -1,5 +1,3 @@
-require('dotenv').config({ path: '../Backend/.env' });
-
 export const frontend = '';
 // export const backend = 'http://localhost:4000';
-export const backend = process.env.BACKEND_URL;
+export const backend = 'https://gradeify.onrender.com';
